07b36bf43b0b18c89ac0f11ea70770dfe92c6a8b
peileppe <peileppeproduction@gmail.com>
2019-06-26 11:36:08 +0200

    -- simple Arena game using pygame and python3

---
+++ sa_menu.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+# sa_menu.py 
+# ===================================================================
+import pygame, time
+
+def set_Display( w,c,f ):
+    global winDisplay
+    global clock
+    global Font
+    winDisplay=w
+    clock=c
+    Font=f
+    return
+
+def show_Report(r):
+    green = (10,100,10)
+    y=5
+    for line in r:
+        y, x = show_Text(line, y, 200, white, len(line)*8)
+        if y > 640: 
+            y=5
+            press_anyKey('more...')
+    press_anyKey()
+    return
+
+def show_Zone(color, x, y, wh, hh):
+    pygame.draw.rect(winDisplay, color, [x, y, wh, hh])
+    return
+
+def show_Text(msg, y=0, x=0, color= (205,205,205), wh=100):
+    global Font
+    black = (10,10,10)
+    show_Zone(color, x,y,wh,15)
+    font = pygame.font.SysFont(None, 25)
+    text = font.render(msg, True, black)
+    winDisplay.blit(text,(x,y))
+    y+=Font.get_height()
+    pygame.display.update()
+    return y, x
+
+def menu_loop(menu):
+    global winDisplay
+    global black
+    global red
+    global blue
+    global white
+    white = (205,205,205)
+    global Font
+    Font = pygame.font.Font(None, 26)
+    black = (10,10,10)
+    red = (195,110,50)
+    blue = (105,175,205)
+    menuExit = False
+    posx = 50
+    posy = 55
+    r=0
+    num_menu=[]
+    for i in range(len(menu)):
+        num_menu.append(48+i)
+    while not menuExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    r-=1
+                if event.key == pygame.K_DOWN:
+                    r+=1
+                if event.key in (13, 32):
+                    menuExit=True
+                if event.key in (27, 113):
+                    r=len(menu)-1
+                    menuExit=True
+                if event.key in num_menu:
+                    r=event.key-48
+                    menuExit=True
+        if r<=0: r=0
+        if r>=len(menu):r=0
+        pygame.draw.rect(winDisplay, black , [posx, posy, len(max(menu,key=len))*8+40, 15*len(menu)+10])
+        r=display_menu(posx+5,posy+5,menu,r)
+        clock.tick(5)
+    return r, menu[r]
+
+def display_menu(x1,y1,menu1, current_option):
+    wh=len(max(menu1,key=len))*8+30
+    for i,o in enumerate(menu1):
+        o=str(i)+". "+o
+        if i==current_option:
+            show_Text(o,y1,x1,white,wh)
+        else:
+            show_Text(o,y1,x1,blue, wh)
+        y1+=15
+    return current_option
+
+def main():
+    global winDisplay
+    global clock
+    global white
+    global Font
+    pygame.init()
+    Font = pygame.font.Font(None, 26)
+    display_width = 800
+    display_height = 600
+    winDisplay = pygame.display.set_mode((display_width,display_height))
+    pygame.display.set_caption('mokinator')
+    white = (205,205,205)
+    winDisplay.fill(white)
+    clock = pygame.time.Clock()
+    i=[0,'new']
+    turn=0
+    MENU=['new','load','save','combat','help','Quit']
+    while i[1] != ('Quit'):
+        i=menu_loop(MENU)
+        turn+=1
+    pygame.quit()
+    quit()
+    return
+
+if __name__ == "__main__":
+    main()
+
---
+++ simpleArena.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+# simpleArena (pygame)
+# ===================================================================
+import pygame
+import random
+import shelve
+import sa_menu
+
+class unit(object):
+    def __init__(self, lbl = 'name', vl = 0):
+        self.name=lbl # name
+        self.hp=vl # hit points 
+        self.mxvalue=vl # max hp
+        return
+    def display(self):
+        return self.name+' '+str(self.hp)
+
+class opponent(unit):
+    def __init__(self, lbl, vl):
+        unit.__init__(self, lbl, vl)
+        l=(0,1,2,3)
+        self.level = random.choice(l)
+        return
+    def damage(self):
+        d=(5,6,7)
+        dmg=random.choice(d)
+        sa_menu.show_Text('D:'+str(dmg),460,400)
+        pygame.display.update()
+        return dmg
+    def show(self):
+        bgc=(105,175,205)
+        sa_menu.show_Zone(bgc,380,350,250,150)
+        sa_menu.show_Text('MONSTER:'+self.name,370,400,(200,200,200),200)
+        sa_menu.show_Text('HP',400,400)
+        sa_menu.show_Text('LV',400,440)
+        sa_menu.show_Text(str(self.hp),430,400)
+        sa_menu.show_Text(str(self.level),430,440)
+        pygame.display.update()
+        return
+    
+class hero(unit):
+    def __init__(self, lbl, vl):
+        unit.__init__(self, lbl,vl)
+        self.level = 0
+        self.xp = 0
+        self.name_pos=(170,400)
+        self.hp_pos=(230,400)
+        self.level_pos=(230,420)
+        self.xp_pos=(230,440)
+        self.txt_pos=(230,440)
+        return
+    def levelUp(self):
+        self.level=int(self.xp/10)
+        self.hp=int(self.level)*4+self.mxvalue
+        return
+    def damage(self):
+        return self.level+5
+    def show(self):
+        bgc=(105,175,205)
+        sa_menu.show_Zone(bgc,380,150,250,150)
+        sa_menu.show_Text('NAME:'+self.name,170,400,(200,200,200),200)
+        sa_menu.show_Text('HP',200,400)
+        sa_menu.show_Text('LV',200,440)
+        sa_menu.show_Text('XP',200,480)
+        sa_menu.show_Text(str(self.hp),230,400)
+        sa_menu.show_Text(str(self.level),230,440)
+        sa_menu.show_Text(str(self.xp),230,480)
+        pygame.display.update()
+        return
+
+def New():
+    global h
+    h = hero('Hero',10)
+    return h
+
+def Load():
+    global h #super important!
+    db=shelve.open('saves/sa_saves')
+    h=db['Hero']
+    db.close()
+    return('load')
+
+def Save():
+    db=shelve.open('saves/sa_saves')
+    db['Hero']=h
+    db.close()
+    return('Save')
+
+def Display():
+    r=h.display()+' '+str(h.level)
+    return r
+
+def Fight():
+    opponent_l=('gladiator','troglodyte','orc','barbarian')
+    o=opponent(random.choice(opponent_l),8)
+    roundr=1
+    while o.hp>=0 and roundr<10:
+        o.show()
+        if (h.hp<=0):
+            return 'Hero is dead'
+        else:
+            h.xp+=1
+            o.hp-=h.damage()
+            h.hp-=o.damage()
+        roundr+=1
+    h.levelUp()
+    return o.name+' is dead'
+
+def Quit():
+    return('Bye')
+
+def main():
+    pygame.init()
+    display_width = 800
+    display_height = 600
+    winDisplay = pygame.display.set_mode((display_width,display_height))
+    pygame.display.set_caption('simple Arena')
+    Font = pygame.font.Font(None, 26)
+    white = (205,205,205)
+    winDisplay.fill(white)
+    clock = pygame.time.Clock()
+    sa_menu.set_Display(winDisplay, clock, Font)
+    turn = 0
+    i = (0,'?')
+    global h
+    h=New()
+    MENU= ['New','Load','Save','Display','Fight','Quit']
+    CMD = [New,Load,Save,Display, Fight, Quit]
+    while i[1] != ('Quit'):
+        h.show()
+        i=sa_menu.menu_loop(MENU)
+        CMD[i[0]]()
+        turn+=1
+    pygame.quit()
+    quit()
+    return
+
+if __name__ == "__main__":
+    main()
+
